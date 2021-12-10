9c70e79e85bfdb2705a4efcb32dba2c9924422f4
Mayank Srivastava <mayank7924@gmail.com>
2021-12-11 00:34:58 +0530

    drawer component mui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
+import * as React from "react";
 import First from "./First";
-import './CSS/App.css'
+import "./CSS/App.css";
 import Second from "./Second";
-import yellowpot from './imgs/yellowpot.jpg'
-import redpot from './imgs/redpot.jpg'
-import bluepot from './imgs/bluepot.jpg'
-import Drawer from "./Drawer";
+import yellowpot from "./imgs/yellowpot.jpg";
+import redpot from "./imgs/redpot.jpg";
+import bluepot from "./imgs/bluepot.jpg";
+import AddIcon from "@mui/icons-material/Add";
+import Button from "@mui/material/Button";
+import Drawer from "@mui/material/Drawer";
+import TemporaryDrawer from "./Drawer.jsx";
 
 function App() {
+  const [state, setState] = React.useState(false);
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setState(open);
+  };
+
   return (
     <div className="App">
       <h1>Welcome to Our Store</h1>
       <First />
       <h2>FEATURED COLLECTIONS</h2>
-      <Second img1={yellowpot} img2={redpot} img3={bluepot} name1={"Yellow Pot"} name2={"Red Pot"} name3={"Blue Pot"}
-
-        price1={"$100"} price2={"$150"} price3={"$90"} />
-      <Drawer />
+      <Button onClick={toggleDrawer(true)}>
+        <AddIcon fontSize="large" />
+        <Drawer open={state} toggleDrawer={toggleDrawer} onClose={toggleDrawer(false)}>
+          <TemporaryDrawer toggleDrawer={toggleDrawer} />
+        </Drawer>
+      </Button>
+      <Second
+        img1={yellowpot}
+        img2={redpot}
+        img3={bluepot}
+        name1={"Yellow Pot"}
+        name2={"Red Pot"}
+        name3={"Blue Pot"}
+      />
     </div>
   );
 }
